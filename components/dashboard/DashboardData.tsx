ff6122cd3dbaf254e1d9a91d6da70800868f65ff
calrosdsa <carlosduram71@gmail.com>
2023-02-22 10:28:08 -0400

    changes

--- components/dashboard/DashboardData.tsx
+++ components/dashboard/DashboardData.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Id, toast } from "react-toastify";
-import { donwloadReportLastTenDays } from "../../context/actions/dashboardActions";
+import { donwloadReportLastTenDays, donwloadReportLastTenDaysExcel } from "../../context/actions/dashboardActions";
 import { useAppDispatch, useAppSelector } from "../../context/reduxHooks";
 import { ReporteId } from "../../data/models/redux-models/dashboard-model";
 import LikeWeekChart from "./charts/LikeWeekChart";
@@ -31,6 +31,26 @@ const DashboardData = ()=>{
             dispatch(donwloadReportLastTenDays(userwifi,idProgress,id,source))
         }
     }
+
+    // const downloadReportExcel = (userwifi:string,idProgress:number) =>{
+    //     const cancelToken = axios.CancelToken;
+    //     const source = cancelToken.source();
+    //         if(dashboardState.ongoingProcess.includes(idProgress)){
+    //             toast.info("Hay una descarga en curso.")
+    //         }else{
+    //         let id:Id;
+    //             id = toast.loading(
+    //             <div className="grid">
+    //                 <span className="text-xs">{ReporteId.ALL_USER == idProgress ? "Descargando Reporte General.Porfavor espere..."
+    //                 :"Descargando Reporte (solo usuarios de la red).Porfavor espere..."
+    //                 }</span>
+    //                <button onClick={()=>source.cancel("Request cancelada")}
+    //                className="text-xs button">Cancelar Descarga</button>
+    //             </div>
+    //             )
+    //         dispatch(donwloadReportLastTenDaysExcel(userwifi,idProgress,id,source))
+    //     }
+    // }
    
 
     return(
@@ -52,9 +72,17 @@ const DashboardData = ()=>{
      </svg>
      <span className=" whitespace-nowrap text-sm font-semibold">Descargar reporte general</span>
            </div>
+
+           {/* <div onClick={()=>downloadReportExcel("0",ReporteId.ALL_USER)}
+           className='button w-min flex  space-x-2'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+     </svg>
+     <span className=" whitespace-nowrap text-sm font-semibold">Descargar reporte general (Excel)</span>
+           </div> */}
+
+
         </div>
-
-
         </div>
 
 
--- pages/user/solicitudes.tsx
+++ pages/user/solicitudes.tsx
@@ -1,17 +1,42 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { FallingLines } from "react-loader-spinner";
+import { Id, toast } from "react-toastify";
 import Layout from "../../components/layout/Layout";
 import DropDown from "../../components/user/solicitudes/DropDown";
 import SolicitudOption from "../../components/user/solicitudes/SolicitudOption";
 import TableSolicitudes from "../../components/user/solicitudes/TableSolicitudes";
+import { donwloadReportLastTenDaysExcel } from "../../context/actions/dashboardActions";
 import { getSolicitudList } from "../../context/actions/userActions";
 import { useAppDispatch, useAppSelector } from "../../context/reduxHooks";
+import { ReporteId } from "../../data/models/redux-models/dashboard-model";
 
 
 const Solicitudes = ()=>{
     const uiState = useAppSelector(state=>state.ui)
     const userState = useAppSelector(state=>state.user)
+    const dashboardState = useAppSelector(state=>state.dashboard)
     const dispatch = useAppDispatch()
+
+    const downloadReportExcel = (userwifi:string,idProgress:number) =>{
+      const cancelToken = axios.CancelToken;
+      const source = cancelToken.source();
+          if(dashboardState.ongoingProcess.includes(idProgress)){
+              toast.info("Hay una descarga en curso.")
+          }else{
+          let id:Id;
+              id = toast.loading(
+              <div className="grid">
+                  <span className="text-xs">{ReporteId.ALL_USER == idProgress ? "Descargando Reporte General.Porfavor espere..."
+                  :"Descargando Reporte (solo usuarios de la red).Porfavor espere..."
+                  }</span>
+                 <button onClick={()=>source.cancel("Request cancelada")}
+                 className="text-xs button">Cancelar Descarga</button>
+              </div>
+              )
+          dispatch(donwloadReportLastTenDaysExcel(userwifi,idProgress,id,source))
+      }
+  }
 
     useEffect(()=>{
       dispatch(getSolicitudList())
@@ -20,8 +45,14 @@ const Solicitudes = ()=>{
     return(
         <>
         <Layout title="Paginas Splash">
-         
             <div className="xl:px-10 xl:pt-4 noselect">
+        <div onClick={()=>downloadReportExcel("0",ReporteId.ALL_USER)}
+           className='button w-min flex  space-x-2 my-4'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+     </svg>
+     <span className=" whitespace-nowrap text-sm font-semibold">Descargar reporte general (Excel)</span>
+           </div>
               <SolicitudOption
               params={userState.params}
               ids={userState.ids}
