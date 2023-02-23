8224bbd5672e5837bc0965935a8b250d10db5c31
calrosdsa <carlosduram71@gmail.com>
2023-02-23 10:39:49 -0400

    changes

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -40,8 +40,9 @@ export default async function auth(req:NextApiRequest,res:NextApiResponse){
             })
         } 
         catch(err:any) {
-            console.log(err.response.statusCode)
-            console.log(err.response.body)
+            console.log(err)
+            // console.log(err.response.statusCode)
+            // console.log(err.response.body)
             const errorMessage = JSON.parse(err.response.body);
                 res.status(err.response.statusCode).json({
                     error: errorMessage
