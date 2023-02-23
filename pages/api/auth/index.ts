6424838389c2456bb554643435db31047effeb71
calrosdsa <carlosduram71@gmail.com>
2023-02-23 10:46:32 -0400

    changes

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -18,12 +18,9 @@ export default async function auth(req:NextApiRequest,res:NextApiResponse){
                     password: password
                 },
             };
-           const formData = new FormData()
-           formData.append("email",email)
-           formData.append("password",password)
-            const response = await axios.post('https://teclu.com/apiFB/public/auth/login',formData)
-            // const data = JSON.parse(response);
-            const data = response.data
+           
+            const response = await rp.post(options)
+            const data = JSON.parse(response);
             setCookie('access_token', data.access_token, { req, res,
                 httpOnly:true,
                 secure: process.env.NODE_ENV !== 'development',
