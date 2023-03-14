2d7664bcb63f137791cf0aa5b61c24d5b6f2d825
calrosdsa <carlosduram71@gmail.com>
2023-03-14 16:14:41 -0400

    changes

--- src/components/user/user-wifi/TableUserWifi.tsx
+++ src/components/user/user-wifi/TableUserWifi.tsx
@@ -54,17 +54,11 @@ const TableUserWifi = ({users,ids}:Props) =>{
                     Mail
                 </th>
                 <th scope="col" className="paddingTable">
-                    Estado
+                    Conexiones
                 </th>
-                <th scope="col" className="paddingTable " onClick={()=>changeOrderList(orderState)}>
-                    <div className=" flex items-center">
-                    Fecha
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 ml-1" 
-                     aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"/></svg>
-                     </div>
-                </th>
+               
                 <th scope="col" className="px-20 xl:px-10 py-3 w-[200px]">
-                    Motivo
+                    Dispositivos
                 </th>
             </tr>
         </thead>
@@ -89,16 +83,16 @@ const TableUserWifi = ({users,ids}:Props) =>{
                 <td className="paddingTable whitespace-nowrap">
                     {item.cantConexion}
                 </td>
-                <td className="paddingTable whitespace-nowrap">
+                {/* <td className="paddingTable whitespace-nowrap">
                     {item.gender}
-                    {/* {formatDate(item.)} */}
-                </td>
+                </td> */}
                 <td  className="paddingTable font-medium text-gray-900 w-[200px] text-xs">
                     {/* <LongText */}
                     {/* content={item.message} */}
                     {/* limit={140} */}
                     {/* /> */}
-                    {formatShortDate(item.birthday)}
+                    {/* {formatShortDate(item.birthday)} */}
+                    {item.cantDispositivo}
 
                 </td>
             </tr>
